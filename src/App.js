308f5d57d844101e0f742d05da14683572dc224c
iann muller <iann@vakinha.com.br>
2019-01-28 01:02:27 -0200

    criado tela de detalhes e respectiva navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from './components/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+       <NavBar />
         {this.props.children}
       </div>
     );
--- src/views/Home.js
+++ src/views/Home.js
@@ -4,44 +4,36 @@ import { hashHistory } from 'react-router';
 import mockData from '../mock/mockImages';
 
 export default class Home extends Component {
-
   render() {
     return (
       <div className="container text-center">
         <div className="row">
           <Card
-            idMovie={mockData[0].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[0].slug}`)}
             img={mockData[0].img}
           />
           <Card
-            idMovie={mockData[1].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[1].slug}`)}
             img={mockData[1].img}
           />
           <Card
-            idMovie={mockData[2].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[2].slug}`)}
             img={mockData[2].img}
           />
           <Card
-            idMovie={mockData[3].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[3].slug}`)}
             img={mockData[3].img}
           />
           <Card
-            idMovie={mockData[4].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[4].slug}`)}
             img={mockData[4].img}
           />
           <Card
-            idMovie={mockData[5].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[5].slug}`)}
             img={mockData[5].img}
           />
           <Card
-            idMovie={mockData[6].slug}
-            action={() => hashHistory.push('/details')}
+            action={() => hashHistory.push(`/details?idMovie=${mockData[6].slug}`)}
             img={mockData[6].img}
           />
         </div>
